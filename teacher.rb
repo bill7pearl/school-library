49dfaa5af25dbc5b3f3b8e1fe53098e885bcae12
Billal <billal7chami@gmail.com>
2023-03-24 08:50:06 +0100

    Fix linters

--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,12 @@
 class Teacher < Person
-    attr_reader :specialization
-  
-    def initialize(age, specialization, name: 'Unknown')
-      super(age, name: name)
-      @specialization = specialization
-    end
-  
-    def can_use_services?
-      true
-    end
+  attr_reader :specialization
+
+  def initialize(age, specialization, name: 'Unknown')
+    super(age, name: name)
+    @specialization = specialization
   end
-  
-  
+
+  def can_use_services?
+    true
+  end
+end
